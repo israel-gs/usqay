45333aa00a18024f023427bbf47d04bbf721bec3
israel-gs <israelgs83@gmail.com>
2019-07-03 14:12:27 -0500

    Adding function for create repository

--- src/commands.js
+++ src/commands.js
@@ -1,12 +1,26 @@
 const program = require('commander')
-const { packageInfo, setToken, setDefaultProjectsPath, setGitUserName, } = require('./utils')
+const {
+    packageInfo,
+    setToken,
+    setDefaultProjectsPath,
+    setGitUserName,
+    getToken,
+    getDefaultProjectsPath,
+    getGitUserName,
+    createNewRepository,
+    deleteRepository,
+    gitIgnore,
+    createFolder,
+    createGitIgnore,
+    existFolder } = require('./utils')
+const { exec } = require('child_process')
+const { join } = require('path')
 
 program
     .version(packageInfo.version, '-v, --version')
 
 program
     .option('-t, --config-token <token>', `Config oAuth token from github`, (token) => {
-        console.log(token)
         setToken(token)
     })
     .option('-p, --config-path <path>', `Config default projects path`, (path) => {
@@ -14,6 +28,70 @@ program
     })
     .option('-u, --config-username <username>', `Config your git username`, (username) => {
         setGitUserName(username)
+    })
+    .option('-c, --create <projectName>', `Config your git username`, (projectName) => {
+
+        const hasToken = getToken()
+        const hasDefaultProjectPath = getDefaultProjectsPath()
+        const hasGitUserName = getGitUserName()
+
+        if (!hasToken || !hasDefaultProjectPath || !hasGitUserName) {
+            console.log('You have not yet configured the token, project path or git username, please use --help command to get help')
+        } else {
+            createNewRepository(projectName, hasToken)
+                .then(res => {
+                    const remoteUrl = res.data.clone_url
+                    const projectPath = join(hasDefaultProjectPath, projectName)
+
+                    if (!existFolder(projectPath)) {
+                        createFolder(projectPath)
+                        exec(`git init ${projectPath} && 
+                          cd ${projectPath} && git remote add origin ${remoteUrl}`, (error, stdout, stderr) => {
+                                if (error) {
+                                    deleteRepository(projectName, hasGitUserName, hasToken)
+                                        .then((res) => {
+                                            console.log('The repository was deleted because an error occurred.')
+                                        })
+                                        .catch((err) => { console.error(err) })
+                                    console.error(error)
+                                }
+                                createGitIgnore(join(projectPath, '.gitignore'), gitIgnore)
+                                exec(`cd ${projectPath} &&
+                                  git add . &&
+                                  git commit -m "Initial Commit" &&
+                                  git push origin master`, (error, stdout, stderr) => {
+                                        if (error) {
+                                            deleteRepository(projectName, hasGitUserName, hasToken)
+                                                .then((res) => {
+                                                    console.log('The repository was deleted because an error occurred.')
+                                                })
+                                                .catch((err) => { console.error(err) })
+                                            console.error(error)
+                                        }
+                                        console.log('Finnished')
+                                    })
+                            })
+
+                    } else {
+                        console.log('A directory with that name already exists')
+                        deleteRepository(projectName, hasGitUserName, hasToken)
+                            .then((res) => {
+                                console.log('The repository was deleted because an error occurred.')
+                            })
+                            .catch((err) => { console.error(err) })
+                    }
+
+                })
+                .catch((err) => {
+                    console.error(err)
+                    deleteRepository(projectName, hasGitUserName, hasToken)
+                        .then((res) => {
+                            console.log('The repository was deleted because an error occurred.')
+                        })
+                        .catch((err) => { console.error(err) })
+                })
+        }
+
     })
 
 program.parse(process.argv)
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,84 @@
 const fs = require('fs')
 const { join } = require('path')
-
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 const packageInfo = JSON.parse(fs.readFileSync(join(__dirname, '..', 'package.json'), { encoding: 'utf-8' }))
+const Octokit = require('@octokit/rest')
+const adapter = new FileSync('src/db.json')
+const db = low(adapter)
+
+db.defaults({
+    config: {
+        token: '',
+        defaultProjectPath: '',
+        gitUserName: ''
+    }
+}).write()
 
 const setToken = (token) => {
-    console.log(token)
+    db.set('config.token', token).write()
 }
+
+const getToken = () => db.get('config.token').value()
 
 const setDefaultProjectsPath = (pathname) => {
-    console.log(pathname)
+    db.set('config.defaultProjectPath', pathname).write()
 }
 
+const getDefaultProjectsPath = () => db.get('config.defaultProjectPath').value()
+
 const setGitUserName = (username) => {
-    console.log(username)
+    db.set('config.gitUserName', username).write()
 }
+
+const getGitUserName = () => db.get('config.gitUserName').value()
+
+const existFolder = (pathname) => fs.existsSync(pathname)
+
+const createFolder = (pathname) => fs.mkdirSync(pathname, { recursive: true })
+
+const createNewRepository = (repoName, token, options) => {
+    const octokit = Octokit({
+        auth: token,
+        userAgent: `${packageInfo.name} ${packageInfo.version}`,
+    })
+
+    return octokit.repos.createForAuthenticatedUser({
+        name: repoName,
+        ...options
+    })
+}
+
+const deleteRepository = (repoName, owner, token) => {
+    const octokit = Octokit({
+        auth: token,
+        userAgent: `${packageInfo.name} ${packageInfo.version}`,
+    })
+
+    return octokit.repos.delete({
+        owner,
+        repo: repoName
+    })
+}
+
+const createGitIgnore = (pathname, data) => {
+    return fs.writeFileSync(pathname, data, { encoding: 'utf-8' })
+}
+
+const gitIgnore = fs.readFileSync(join(__dirname, 'gitignore.txt'), { encoding: 'UTF-8' })
 
 module.exports = {
     packageInfo,
     setToken,
     setDefaultProjectsPath,
     setGitUserName,
+    getToken,
+    getDefaultProjectsPath,
+    getGitUserName,
+    createNewRepository,
+    deleteRepository,
+    gitIgnore,
+    existFolder,
+    createFolder,
+    createGitIgnore
 }
